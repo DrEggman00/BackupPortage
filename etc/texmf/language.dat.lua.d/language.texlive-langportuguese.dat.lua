74be1d69889350663074f16a24029a91a7a1baee
vinicius.daniel <vinicius.daniel@uft.edu.br>
2026-01-13 11:26:34 -0300

    Update 13-01

--- etc/texmf/language.dat.lua.d/language.texlive-langportuguese.dat.lua
+++
@@ -1,8 +0,0 @@
-	['portuguese'] = {
-		loader = 'loadhyph-pt.tex',
-		lefthyphenmin = 2,
-		righthyphenmin = 3,
-		synonyms = { 'portuges' },
-		patterns = 'hyph-pt.pat.txt',
-		hyphenation = 'hyph-pt.hyp.txt',
-	},
